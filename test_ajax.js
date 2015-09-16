a31e0e878edbc0784d291e83c1576b174d7f2541
Linz Jax <linzjax@gmail.com>
2015-09-16 17:47:44 -0400

    Successfully filtering out food words.
    the USDA api is now successfully filterning out food words from an array and (thanks to promises) is now putting them back together in order.
    
    Now I have to tear apart my original program in order to incorporate it and add more promises to the rest of my filter.

--- test_ajax.js
+++ test_ajax.js
@@ -1,52 +1,60 @@
 "use strict";
-var ingredients = ["1", "tablespoon", "vanilla", "sweet","potato"];
-var ingredientString = "";
 
 
 var isItFood = function(searchItem){
 	return new Promise(function(resolve, reject){
-		var url = "http://api.nal.usda.gov/ndb/search/?format=json&q=" + searchItem + "&api_key=gCd9veMtkhLO0qPpxbfOUP16GJ9s9sQnUdo8Ysl7";
+		var url = "http://api.nal.usda.gov/ndb/search/?format=json&q=" + searchItem + "&max=10&api_key=gCd9veMtkhLO0qPpxbfOUP16GJ9s9sQnUdo8Ysl7";
 
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", url);
 
 		xhr.onreadystatechange = function(){
 			if (xhr.status === 404){
-				reject(Error("404 Error"));
+				resolve();
 			}
 			if (xhr.readyState === 4 && xhr.status === 200){
-				var x = 0;
+				var counter = 0;
 				var foodName = '';
 				var	response = JSON.parse(xhr.responseText);
 				response.list.item.filter(function(food){
 					var thing = food.name.toLowerCase().split(' ');
 					if (thing[0].match(searchItem)){
-						x++;
-						console.log(thing);
+						counter++;
 						foodName = thing[0];
 					}
 				});
-				if (x > 0){
-					console.log(foodName);
-					// resolve(foodName);
+				if (counter > 0){
+					resolve(searchItem);
+				} else {
+					resolve();
 				}
 			}//end readyState
 		};//onreadystatechange
 
 		xhr.onerror = function(){
-			reject(Error("Network Error"));
+			resolve();
 		};
 
 		xhr.send();
 	});
 };
 
+var prom = function(ingredients){
 
-ingredients.forEach(function(data){
-	return isItFood(data)
-});
+	return Promise.all(ingredients.map(isItFood)).then(function(values){
+		console.log(values.join(' '));
+		var iString = values.filter(function(item){
+			if (item !== '')
+				return item;
+		}).join(' ');
+		console.log(iString);
+
+	});
+};
 
 
+
+prom(['bear','colon','cancer','fruit']);
 
 
 
